5216ac134776bf5658c295632213ab192994b235
Kolja Kühn <kolja@heck-kuehn.de>
2023-02-03 23:01:48 +0100

    Change to old board.

--- main.cpp
+++ main.cpp
@@ -2,14 +2,15 @@
 #include "perft.h"
 #include "chess.hpp"
 #include "sequential_search.h"
+#include "chess-library/src/chess.hpp"
 
 
 int main() {
     init_tables();
 
-    Eval_Board board;
+    Board board;
     Search<true> search(board);
-    for (int depth = 1; depth < 10; depth++) {
+    for (int depth = 1; depth < 9; depth++) {
         search.root_max(INT32_MIN / 2, INT32_MAX / 2, depth);
     }
     std::cout << board.eval() << std::endl;
