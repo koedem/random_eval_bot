9e7c4642435b41a71fda12dcd4758be45635b453
Kolja Kühn <kolja@heck-kuehn.de>
2023-02-05 23:00:52 +0100

    Move TT helper structs out of the TT class. Add print_pv method.

--- transposition_table.h
+++ transposition_table.h
@@ -5,30 +5,30 @@
 #include <vector>
 #include "chess-library/src/chess.hpp"
 
+enum Bound_Type : uint8_t {
+    UPPER_BOUND, LOWER_BOUND, EXACT
+};
+
+struct TT_Info {
+    int32_t eval;
+    Chess::Move move;
+    int8_t depth;
+    Bound_Type type;
+
+    bool operator<(TT_Info& other) const {
+        if (type == EXACT && other.type != EXACT) {
+            return false;
+        } else if (type != EXACT && other.type == EXACT) {
+            return true;
+        }
+        return depth < other.depth;
+    }
+};
+
 class Transposition_Table {
 
 private:
     static constexpr uint32_t entries_per_bucket = 4;
-
-    enum Bound_Type : uint8_t {
-        ALPHA, BETA, EXACT
-    };
-
-    struct TT_Info {
-        Chess::Move move;
-        int16_t eval;
-        int8_t depth;
-        Bound_Type type;
-
-        bool operator<(TT_Info& other) const {
-            if (type == EXACT && other.type != EXACT) {
-                return false;
-            } else if (type != EXACT && other.type == EXACT) {
-                return true;
-            }
-            return depth < other.depth;
-        }
-    };
 
     struct Entry {
         uint64_t key;
@@ -69,11 +69,11 @@ public:
         }
     }*/
 
-    void emplace(uint64_t key, TT_Info value, uint32_t depth) {
-        auto & entries = table[pos(key + depth)].entries;
+    void emplace(uint64_t key, TT_Info value, int32_t depth) {
+        auto & entries = table[pos(key, depth)].entries;
         for (auto & entry : entries) { // Check if the entry already exists
             if (entry.key == key) {
-                assert(entry.info.depth == depth);
+                assert(entry.value.depth == depth);
                 assert(value.depth == depth);
                 entry.value = value;
                 return;
@@ -109,8 +109,8 @@ public:
         missed_writes += value == 0 ? 0 : 1;
     }*/
 
-    [[nodiscard]] TT_Info at(uint64_t key) const {
-        for (auto& entry : table[pos(key)].entries) {
+    [[nodiscard]] TT_Info at(uint64_t key, int32_t depth) const {
+        for (auto& entry : table[pos(key, depth)].entries) {
             if (entry.key == key) {
                 return entry.value;
             }
@@ -118,8 +118,8 @@ public:
         return TT_Info{};
     }
 
-    bool contains(uint64_t key) {
-        for (auto& entry : table[pos(key)].entries) { // NOLINT(readability-use-anyofallof)
+    bool contains(uint64_t key, int32_t depth) {
+        for (auto& entry : table[pos(key, depth)].entries) { // NOLINT(readability-use-anyofallof)
             if (entry.key == key) {
                 return true;
             }
@@ -127,12 +127,27 @@ public:
         return false;
     }
 
-    static inline uint64_t pos(uint64_t key) {
-        return key % size; // this is a compile-time constant and gets compiled to either a bit and or an efficient version of this
+    void print_pv(Board& board, int depth) {
+        Board copy(board);
+        while (depth > 0) {
+            if (contains(copy.hashKey, depth)) {
+                Move move = at(copy.hashKey, depth).move;
+                std::cout << convertMoveToUci(move) << " ";
+                copy.makeMove(move);
+                depth--;
+            } else {
+                break;
+            }
+        }
+        std::cout << std::endl;
+    }
+
+    static inline uint64_t pos(uint64_t key, int32_t depth) {
+        return (key + depth) % size; // this is a compile-time constant and gets compiled to either a bit and or an efficient version of this
     }
 
 private:
-    static constexpr uint32_t size = 2 << 17;
+    static constexpr uint32_t size = 1 << 27;
     std::vector<Bucket> table;
 
     uint64_t missed_writes = 0;
