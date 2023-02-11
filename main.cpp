a4d8acdd45ec7085b582cccc31ae7aff82416ec8
Kolja Kühn <kolja@heck-kuehn.de>
2023-02-11 10:45:20 +0100

    Create ABDADA search in main.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include "perft.h"
 #include "sequential_search.h"
 #include "simple_concurrent_search.h"
+#include "abdada_tt.h"
+#include "abdada_search.h"
 
 struct Search_Result {
     uint64_t nodes = 0;
@@ -24,14 +26,17 @@ struct Search_Result {
 int main() {
     Board board;
     Transposition_Table<REPLACE_LAST_ENTRY> table(16);
-    Locking_TT<REPLACE_LAST_ENTRY> locking_tt(16384);
+    Locking_TT<REPLACE_LAST_ENTRY> locking_tt(256);
 
     Search<true, REPLACE_LAST_ENTRY> search(board, table);
     size_t num_threads = 11;
     Lazy_SMP<true, REPLACE_LAST_ENTRY> lazy_smp(num_threads, board, locking_tt);
-    for (int iteration = 0; iteration < 10; iteration++) {
-        int up_to_depth = 15;
-        lazy_smp.parallel_search<Search_Result, true>(up_to_depth);
+
+    ABDADA_TT<REPLACE_LAST_ENTRY> abdada_tt(256);
+    ABDADA_Search<true, REPLACE_LAST_ENTRY> abdada(num_threads, board, abdada_tt);
+    for (int iteration = 0; iteration < 20; iteration++) {
+        int up_to_depth = 11;
+        lazy_smp.parallel_search<Search_Result, true>(up_to_depth, iteration);
             //result.print_table(iteration);
             //locking_tt.print_size();
             //locking_tt.print_pv(board, depth);
