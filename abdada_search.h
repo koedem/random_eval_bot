0002721159e0f052e55b374bb52e8607ae9cf913
Kolja Kühn <kolja@heck-kuehn.de>
2023-02-11 16:52:13 +0100

    Continue implementation and use the new TT inc/dec options.

--- abdada_search.h
+++ abdada_search.h
@@ -4,8 +4,7 @@
 #include <functional>
 #include "locking_tt.h"
 #include "abdada_tt.h"
-
-constexpr Eval_Type ON_EVALUATION = INT16_MIN;
+#include "compile_time_constants.h"
 
 template<bool Q_SEARCH, TT_Strategy strategy>
 class alignas (128) ABDADA_Thread { // Let's go big with the alignas just in case
@@ -25,10 +24,16 @@ private:
      * @return true if the TT probe produced a cutoff, i.e. the search can be skipped, and the TT entry value,
      * stored in alpha, can be returned.
      */
-    bool tt_probe(Move& move, Eval_Type& alpha, Eval_Type& beta, int depth) {
-        Locked_TT_Info tt_entry{};
-        if (tt.get_if_exists(board.hashKey, depth, tt_entry)) {
+    bool tt_probe(Move& move, Eval_Type& alpha, Eval_Type& beta, int depth, bool exclusive) {
+        ABDADA_TT_Info tt_entry{};
+        if (tt.template get_if_exists<true>(board.hashKey, depth, tt_entry, exclusive)) {
             assert(tt_entry.depth == depth);
+            assert(tt_entry.eval != ON_EVALUATION);
+
+            if (exclusive && tt_entry.proc_number > 0) {
+                alpha = ON_EVALUATION;
+                return true; // "Cutoff" because another thread is already searching this node.
+            }
 
             if (tt_entry.type == EXACT) {
                 alpha = tt_entry.eval;
@@ -47,14 +52,14 @@ private:
             move = tt_entry.move;
         }
         if (move == NO_MOVE) { // If we didn't find a TT move, try from one depth earlier instead
-            if (tt.get_if_exists(board.hashKey, depth - 1, tt_entry)) {
+            if (tt.template get_if_exists<false>(board.hashKey, depth - 1, tt_entry, exclusive)) {
                 assert(tt_entry.depth == depth - 1);
                 move = tt_entry.move;
             }
         }
         return false;
     }
-/*
+
     template<Movetype TYPE>
     void generate_shuffled_moves(Movelist& moves) {
         Movegen::legalmoves<TYPE>(board, moves);
@@ -71,16 +76,16 @@ private:
             moves[i] = randomElement;
         }
     }
-*/
+
 public:
     explicit ABDADA_Thread(Board& board, ABDADA_TT<strategy>& table, std::atomic<bool>& finished)
                                                     : board(board), tt(table), finished(finished) {
-    }/*
+    }
 
     Eval_Type q_search(Eval_Type alpha, Eval_Type beta) {
         Eval_Type q_eval = board.eval();
-        if (q_eval == INT16_MIN) { // Avoid overflow issues when inverting the eval.
-            q_eval++;
+        if (q_eval < MIN_EVAL) { // Avoid overflow issues when inverting the eval.
+            q_eval = MIN_EVAL;
         }
         nodes++;
         if constexpr (!Q_SEARCH) {
@@ -116,8 +121,8 @@ public:
 
     Eval_Type nw_q_search(Eval_Type beta) {
         Eval_Type q_eval = board.eval();
-        if (q_eval == INT16_MIN) { // Avoid overflow issues when inverting the eval.
-            q_eval++;
+        if (q_eval < MIN_EVAL) { // Avoid overflow issues when inverting the eval.
+            q_eval = MIN_EVAL;
         }
         nodes++;
         if constexpr (!Q_SEARCH) {
@@ -145,15 +150,15 @@ public:
         return q_eval;
     }
 
-    Eval_Type null_window_search(Eval_Type beta, int depth) {
-        Eval_Type eval = INT16_MIN + 1;
+    Eval_Type null_window_search(Eval_Type beta, int depth, bool exclusive) {
+        Eval_Type eval = MIN_EVAL;
         Move tt_move = NO_MOVE;
         Eval_Type alpha = beta - 1;
-        if (tt_probe(tt_move, alpha, beta, depth)) { // I.e. if cutoff
+        if (tt_probe(tt_move, alpha, beta, depth, exclusive)) { // I.e. if cutoff
             return alpha; // TT entry value is put here
         }
 
-        Locked_TT_Info entry{eval, tt_move, (int8_t) depth, UPPER_BOUND}; // If we don't find a move, keep the old TT move
+        ABDADA_TT_Info entry{eval, tt_move, (int8_t) depth, UPPER_BOUND, 0}; // If we don't find a move, keep the old TT move
         Movelist moves;
         generate_shuffled_moves<ALL>(moves);
         int tt_move_index = moves.find(tt_move);
@@ -164,7 +169,7 @@ public:
             board.makeMove(move.move);
             Eval_Type inner_eval;
             if (depth > 1) {
-                inner_eval = -null_window_search(-beta + 1, depth - 1);
+                inner_eval = -null_window_search(-beta + 1, depth - 1, false); // TODO move == firstmove
             } else {
                 inner_eval = -nw_q_search(-beta + 1);
             }
@@ -183,18 +188,18 @@ public:
             }
         }
         entry.eval = eval;
-        tt.emplace(board.hashKey, entry, depth);
+        tt.template emplace<true>(board.hashKey, entry, depth);
         return eval;
     }
 
     Eval_Type pv_search(Eval_Type alpha, Eval_Type beta, int depth) {
-        Eval_Type eval = INT16_MIN + 1;
+        Eval_Type eval = MIN_EVAL;
         Move tt_move = NO_MOVE;
-        if (tt_probe(tt_move, alpha, beta, depth)) { // I.e. if cutoff
+        if (tt_probe(tt_move, alpha, beta, depth, false)) { // I.e. if cutoff
             return alpha; // TT entry value is put here
         }
 
-        Locked_TT_Info entry{eval, tt_move, (int8_t) depth, UPPER_BOUND}; // If we don't find a move, keep the old TT move
+        ABDADA_TT_Info entry{eval, tt_move, (int8_t) depth, UPPER_BOUND, 0}; // If we don't find a move, keep the old TT move
         Movelist moves;
         generate_shuffled_moves<ALL>(moves);
         int tt_move_index = moves.find(tt_move);
@@ -202,21 +207,33 @@ public:
             std::swap(moves[0], moves[tt_move_index]); // Search the TT move first
         }
 
+        std::vector<Move> deferred_moves{};
+        deferred_moves.reserve(moves.size);
+
         bool search_full_window = true;
-        for (auto& move : moves) {
-            board.makeMove(move.move);
-            Eval_Type inner_eval;
+        for (int move_index = 0; move_index < moves.size; move_index++) {
+            Move move = moves[move_index].move;
+            board.makeMove(move);
+            Eval_Type inner_eval = MAX_EVAL; // Hack so that further down below inner eval is bigger than alpha if no search was done
             if (depth == 1) {
                 inner_eval = -q_search(-beta, -alpha);
-            } else if (search_full_window || (inner_eval = -null_window_search(-alpha, depth - 1)) > alpha){
-                inner_eval = -pv_search(-beta, -alpha, depth - 1);
-                search_full_window = false;
+            } else {
+                if (!search_full_window) {
+                    inner_eval = -null_window_search(-alpha, depth - 1, true);
+                    if (inner_eval == (Eval_Type) -ON_EVALUATION) {
+                        deferred_moves.emplace_back(move);
+                    }
+                }
+                if (inner_eval > alpha) {
+                    inner_eval = -pv_search(-beta, -alpha, depth - 1);
+                    search_full_window = false;
+                }
             }
-            board.unmakeMove(move.move);
+            board.unmakeMove(move);
 
             if (inner_eval > eval) {
                 eval = inner_eval;
-                entry.move = move.move; // If it stays this way, this is the best move
+                entry.move = move; // If it stays this way, this is the best move
                 if (eval >= beta) {
                     entry.type = LOWER_BOUND;
                     break;
@@ -231,22 +248,64 @@ public:
                 }
             }
         }
+
+        for (auto& move : deferred_moves) { // We never enter this at depth 1, also never search full window right away.
+            board.makeMove(move);
+            Eval_Type inner_eval = -null_window_search(-alpha, depth - 1, false);
+            if (inner_eval > alpha) {
+                inner_eval = -pv_search(-beta, -alpha, depth - 1);
+            }
+            board.unmakeMove(move);
+
+            if (inner_eval > eval) {
+                eval = inner_eval;
+                entry.move = move; // If it stays this way, this is the best move
+                if (eval >= beta) {
+                    entry.type = LOWER_BOUND;
+                    break;
+                }
+                if (eval > alpha) {
+                    alpha = eval;
+                    entry.type = EXACT; // We raised alpha, so it's no longer a lower bound, either exact or upper bound
+                }
+
+                if (finished) { // If someone else already completed the search there is no reason for us to continue
+                    return eval;
+                }
+            }
+        }
+
         entry.eval = eval;
-        tt.emplace(board.hashKey, entry, depth);
+        tt.template emplace<true>(board.hashKey, entry, depth);
         return eval;
     }
 
-    Eval_Type nega_max(Eval_Type alpha, Eval_Type beta, int depth) {
-        Eval_Type eval = INT16_MIN + 1;
+    /**
+     *
+     * @param alpha
+     * @param beta
+     * @param depth
+     * @param exclusive_searched This parameter does double duties. It should be passed in whether this node should be
+     * calculated exclusively. In this case, if another thread already calculates this node, we don't return a result.
+     * However, upon returning this bool contains whether we actually searched the node. If no, this node should be
+     * recalculated later.
+     * @return
+     */
+    /*Eval_Type nega_max(Eval_Type alpha, Eval_Type beta, int depth, bool& exclusive_searched) {
+        Eval_Type eval = MIN_EVAL;
         Move tt_move = NO_MOVE;
         if (tt_probe(tt_move, alpha, beta, depth)) { // I.e. if cutoff
+            exclusive_searched = true;
             return alpha; // TT entry value is put here
         }
 
         Locked_TT_Info entry{eval, tt_move, (int8_t) depth, UPPER_BOUND}; // If we don't find a move, keep the old TT move
         Movelist moves;
         generate_shuffled_moves<ALL>(moves);
-        // TODO why there no hashmove first here?
+        int tt_move_index = moves.find(tt_move);
+        if (tt_move_index > 0) {
+            std::swap(moves[0], moves[tt_move_index]); // Search the TT move first
+        }
         for (auto& move : moves) {
             board.makeMove(move.move);
             Eval_Type inner_eval;
@@ -277,16 +336,16 @@ public:
         entry.eval = eval;
         tt.emplace(board.hashKey, entry, depth);
         return eval;
-    }
+    }*/
 
-    template<class Search_Result, bool PV_Search>
+    template<class Search_Result, bool PV_Search> // TODO implement this function
     void root_max(Eval_Type alpha, Eval_Type beta, int depth, Search_Result& result) {
         auto start = std::chrono::high_resolution_clock::now();
         nodes = 0;
         assert(depth > 0);
-        Eval_Type eval = INT16_MIN + 1;
+        Eval_Type eval = MIN_EVAL;
         Move tt_move = NO_MOVE;
-        if (tt_probe(tt_move, alpha, beta, depth)) { // This can probably never happen but maybe in parallel search
+        if (tt_probe(tt_move, alpha, beta, depth, false)) { // This can probably never happen but maybe in parallel search
             return; // I'm claiming that if this happens, then we already have a search result from another thread so we don't need to return anything
         }
 
@@ -307,8 +366,8 @@ public:
             if (depth == 1) {
                 inner_eval = -q_search(-beta, -alpha);
             } else if constexpr (!PV_Search) {
-                inner_eval = -nega_max(-beta, -alpha, depth - 1);
-            } else if (search_full_window || (inner_eval = -null_window_search(-alpha, depth - 1)) > alpha){
+                //inner_eval = -nega_max(-beta, -alpha, depth - 1);
+            } else if (search_full_window || (inner_eval = -null_window_search(-alpha, depth - 1, true)) > alpha){
                 inner_eval = -pv_search(-beta, -alpha, depth - 1);
                 search_full_window = false;
             }
@@ -335,7 +394,7 @@ public:
                 }
             }
         }
-        tt.emplace(board.hashKey, {eval, best_move, (int8_t) depth, EXACT}, depth);
+        tt.template emplace<true>(board.hashKey, {eval, best_move, (int8_t) depth, EXACT, 0}, depth);
 
         auto end = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double> duration = end - start;
@@ -349,7 +408,7 @@ public:
             result.eval = eval;
             result.depth = depth;
         }
-    }*/
+    }
 };
 
 template<bool Q_SEARCH, TT_Strategy strategy>
@@ -377,8 +436,8 @@ public:
         Search_Result result;
         for (int depth = 1; depth <= up_to_depth; depth++) {
             std::vector<std::thread> search_threads;
-            Eval_Type alpha = INT16_MIN + 1; // Don't use INT16_MIN because negating it causes an overflow
-            Eval_Type beta = INT16_MAX;
+            Eval_Type alpha = MIN_EVAL; // Don't use INT16_MIN because negating it causes an overflow
+            Eval_Type beta = MAX_EVAL;
             finished = false;
             for (size_t i = 0; i < num_threads; i++) {
                 auto func = std::bind(&ABDADA_Thread<Q_SEARCH, strategy>::template root_max<Search_Result, PV_Search>,
