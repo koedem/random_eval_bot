b6a6f435d202c4ba5cdf31c3781cf621c2e942df
Kolja Kühn <kolja@heck-kuehn.de>
2023-02-16 11:22:21 +0100

    Add compile time option for where to print.

--- compile_time_constants.h
+++ compile_time_constants.h
@@ -6,3 +6,4 @@ constexpr bool DEBUG_OUTPUTS = false;
 constexpr Eval_Type MIN_EVAL = std::numeric_limits<int16_t>::min() + 1, MAX_EVAL = std::numeric_limits<int16_t>::max();
 constexpr Eval_Type ON_EVALUATION = std::numeric_limits<int16_t>::min();
 constexpr std::int32_t DEFER_DEPTH = 2;
+constexpr bool PRINT_TO_FILE = true;
